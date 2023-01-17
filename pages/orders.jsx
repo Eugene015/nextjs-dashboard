4d87e115219d9fa989baa1ec9033c8802a065354
Eugene015 <egavriluk@gmail.com>
2023-01-17 15:02:03 +0100

    orders page added

--- pages/orders.jsx
+++ pages/orders.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { FaShoppingBag } from "react-icons/fa";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { data } from "../data/data.js";
 
 function Orders() {
-  return <div>Orders</div>;
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <div className="flex justify-between px-4 pt-4">
+        <h2>Orders</h2>
+        <h2>Welcome back, Eugene</h2>
+      </div>
+      <div className="p-4">
+        <div className="w-full m-auto"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Orders;
