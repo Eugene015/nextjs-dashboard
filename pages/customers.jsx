7a3c5362847f6692302158062394a8c2de744c6c
Eugene015 <egavriluk@gmail.com>
2023-01-17 14:10:14 +0100

    customers page added

---
+++ pages/customers.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BsPersonFill, BsThreeDotsVertical } from "react-icons/bs";
+import { data } from "../data/data.js";
+
+function Customers() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <div className="flex justify-between p-4">
+        <h2>Customers</h2>
+        <h2>Welcome back, Eugene</h2>
+      </div>
+      <div className="p-4"></div>
+    </div>
+  );
+}
+
+export default Customers;
