91a78d456a41d693c03efdb1143fb9286b491a93
Eugene015 <egavriluk@gmail.com>
2023-01-17 15:29:16 +0100

    v1.0.0

--- pages/orders.jsx
+++ pages/orders.jsx
@@ -11,7 +11,52 @@ function Orders() {
         <h2>Welcome back, Eugene</h2>
       </div>
       <div className="p-4">
-        <div className="w-full m-auto"></div>
+        <div className="w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto">
+          <div className="my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer">
+            <span>Order</span>
+            <span className="sm:text-left text-right">Status</span>
+            <span className="hidden md:grid">Last Order</span>
+            <span className="hidden sm:grid">Method</span>
+          </div>
+          <ul>
+            {data.map((order, id) => (
+              <li
+                key={id}
+                className="bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between"
+              >
+                <div className="flex">
+                  <div className="bg-purple-100 p-3 rounded-lg">
+                    <FaShoppingBag className="text-purple-800" />
+                  </div>
+                  <div className="pl-4">
+                    <p className="text-gray-800 font-bold">
+                      ${order.total.toLocaleString()}
+                    </p>
+                    <p className="text-gray-800 text-sm">{order.name.first}</p>
+                  </div>
+                </div>
+                <p className="text-gray-600 sm:text-left text-right">
+                  <span
+                    className={
+                      order.status === "Processing"
+                        ? "bg-green-200 p-2 rounded-lg"
+                        : order.status === "Completed"
+                        ? "bg-blue-200 p-2 rounded-lg"
+                        : "bg-yellow-200 p-2 rounded-lg"
+                    }
+                  >
+                    {order.status}
+                  </span>
+                </p>
+                <p className="hidden md:flex">{order.date}</p>
+                <div className="sm:flex hidden justify-between items-center">
+                  <p>{order.method}</p>
+                  <BsThreeDotsVertical />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
